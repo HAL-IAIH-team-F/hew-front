192c662d4d2435f5c571fe0de10af5a541edd57d
kigawa <account@kigawa.net>
2024-08-07 21:10:10 +0000

    add endless scroll

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,44 +1,38 @@
-"use client"
+"use client";
 import {useWindowSize} from "@/app/_hook/useWindowSize";
 import {useTopScroll} from "@/app/_hook/useTopScroll";
 import {useEffect, useRef, useState} from "react";
+import {TimelineExtender} from "@/app/timeline/extend";
 
-const margin = 100
+const margin = 100;
+let aaa = 0
 export default function Page(
-  {}: {}
+  {}: {},
 ) {
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
-  const [offsetX, setOffsetX] = useState(0)
-  const [offsetY, setOffsetY] = useState(0)
-  const windowSize = useWindowSize()
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
+  const windowSize = useWindowSize();
   const ref = useRef<HTMLDivElement>(null);
-  const scroll = useTopScroll(ref)
+  const scroll = useTopScroll(ref);
 
   useEffect(() => {
-    if (windowSize.height > height) setHeight(windowSize.height)
-    if (windowSize.width > width) setWidth(windowSize.width)
-    if (scroll.top + windowSize.height + margin > height) {
-      setHeight(prevState => prevState + margin)
-    }
-    if (scroll.top < margin) {
-      ref.current?.scrollTo({top: scroll.top + margin})
-      setHeight(prevState => prevState + margin)
-      setOffsetY(prevState => prevState + margin)
-    }
-    if (scroll.left + windowSize.width + margin > width) {
-      setWidth(prevState => prevState + margin)
-    }
-    if (scroll.left < margin) {
-      ref.current?.scrollTo({left: scroll.left + margin})
-      setWidth(prevState => prevState + margin)
-      setOffsetX(prevState => prevState + margin)
-    }
+    if (windowSize.height > height) setHeight(windowSize.height);
+    if (windowSize.width > width) setWidth(windowSize.width);
+    if (scroll.top < margin)
+      TimelineExtender.extendTop(ref.current, scroll, margin, setHeight, setOffsetY, windowSize, height);
+    if (scroll.top + windowSize.height + margin > height)
+      TimelineExtender.extendBottom(setHeight, margin, windowSize, height, ref.current, scroll, setOffsetY);
+    if (scroll.left < margin)
+      TimelineExtender.extendLeft(ref.current, scroll, margin, setWidth, setOffsetX, windowSize, width);
+    if (scroll.left + windowSize.width + margin > width)
+      TimelineExtender.extendRight(setWidth, margin, windowSize, width, ref.current, scroll, setOffsetX);
   }, [windowSize, scroll]);
-  console.debug("width: ", width)
-  console.debug("height: ", height)
-  console.debug("windowSize: ", windowSize)
-  console.debug("scroll: ", scroll)
+  console.debug("width: ", width);
+  console.debug("height: ", height);
+  console.debug("windowSize: ", windowSize);
+  console.debug("scroll: ", scroll);
   return <div
     ref={ref}
     className={"w-[100vw] h-[100vh] overflow-scroll"}
@@ -46,11 +40,13 @@ export default function Page(
     <div
       style={{
         width: `${width}px`,
-        height: `${height}px`
+        height: `${height}px`,
       }}
       className={"relative"}
     >
-      <div className={"h-10 w-10 bg-blue absolute"} style={{top: offsetY, left: offsetX}}></div>
+      <div className={"h-10 w-10 bg-blue absolute"} style={{top: offsetY, left: offsetX}}>
+        {aaa }
+      </div>
     </div>
-  </div>
+  </div>;
 }
