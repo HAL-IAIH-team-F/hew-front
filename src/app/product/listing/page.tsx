cce09823c85fdec9b6db469856ae05976902e1e8
Mao <90289410+injectxr@users.noreply.github.com>
2024-09-07 19:24:27 +0900

    add: ProductListing page

---
+++ src/app/product/listing/page.tsx
@@ -0,0 +1,9 @@
+import Main from "~/Main";
+import ProductListingForm from "@/product/listing/ProductListingForm";
+export default function Page(
+  {}: {}
+) {
+  return <Main>
+    <ProductListingForm/>
+  </Main>
+}
