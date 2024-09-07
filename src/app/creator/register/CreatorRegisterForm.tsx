76c645caa15f6e90db1bd892a314f62ff29f1a19
kigawa01 <contact@kigawa.net>
2024-09-07 07:43:13 +0000

    add creator register form

---
+++ src/app/creator/register/CreatorRegisterForm.tsx
@@ -0,0 +1,23 @@
+import {StyledForm} from "../../../util/form/StyledForm";
+import {StyledInput} from "../../../util/form/StyledInput";
+import FlexBox from "../../../util/FlexBox";
+import {StyledButton} from "../../../util/form/StyledButton";
+
+export default function CreatorRegisterForm(
+  {
+    ...props
+  }: CreatorRegisterFormProps,
+) {
+  return (
+    <StyledForm {...props}>
+      <StyledInput name={"contact_address"} label={"連絡先(一般に表示されます)"}/>
+      <StyledInput name={"transfer_target"} label={"振込先"}/>
+      <FlexBox className={"justify-end px-10"}>
+        <StyledButton>登録</StyledButton>
+      </FlexBox>
+    </StyledForm>
+  )
+}
+
+export interface CreatorRegisterFormProps {
+}
