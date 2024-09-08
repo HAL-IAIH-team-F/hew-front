147f6245aefa1d506945e19b502beab77ec12691
syoumakuwaba <syoumakuwaba@icloud.com>
2024-09-08 19:01:51 +0900

    アイコン変更ボタンの追加

--- src/app/user/register/UserRegisterForm.tsx
+++ src/app/user/register/UserRegisterForm.tsx
@@ -5,44 +5,46 @@ import {StyledButton} from "../../../util/form/StyledButton";
 import FlexBox from "../../../util/FlexBox";
 import {ChangeEvent, useState} from "react";
 
+
 export default function UserRegisterForm(
     {
         ...props
     }: UserRegisterFormProps,
 ) {
-  const [imagePreview, setImagePreview] = useState<string | null>(null);
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
 
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePreview(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreview(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <StyledForm {...props}>
-            <div className="relative w-[100px] h-[100px]">
-                <div className="w-full h-full overflow-hidden rounded-full bg-gray-200">
+            <label className="relative w-fit block">
+                <div className="w-[150px] h-[150px] overflow-hidden rounded-full bg-gray-200">
                     {imagePreview ? (
                         <img src={imagePreview} alt="プレビュー" className="w-full h-full object-cover"/>
                     ) : (
                         <div className="w-full h-full flex items-center justify-center">
-                            Camera
+                            <img className={"w-full h-full"} src={"/curtain.png"}/>
                         </div>
                     )}
                 </div>
+                <div className={"rounded-3xl w-20 mx-auto py-1 hover:bg-lightGray mt-2 bg-white border-2 border-borderDef text-center"}>変更</div>
                 <input name={"icon"}
-                    type="file"
-                    onChange={handleImageChange}
-                    accept="image/*"
-                    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                       type="file"
+                       onChange={handleImageChange}
+                       accept="image/*"
+                       className="invisible h-0 w-0 fixed"
                 />
-            </div>
+            </label>
             <StyledInput name={"メールアドレス"} type={"text"}/>
             <StyledInput name={"パスワード"} type={"text"}/>
             <StyledInput name={"ユーザーネーム"} type={"text"}/>
