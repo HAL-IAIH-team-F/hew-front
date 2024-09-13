cab5e3aa694983e106dbadb4a161bb854fd61507
kigawa01 <contact@kigawa.net>
2024-09-13 06:36:57 +0000

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import {ReactNode} from "react";
 import {StyledNavigation} from "~/Navigation";
-import BackgroundObject from "~/BackgroungObject";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -22,7 +21,7 @@ export default function RootLayout(
     <html lang="en">
       <body className={inter.className + " bg-backcolor"}>
         <StyledNavigation />
-        <BackgroundObject/>
+        {/*<BackgroundObject/>*/}
         {children}
       </body>
     </html>
