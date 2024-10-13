421b24f9177b20c106539ebacd530ab1239c019b
kigawa <kigawa.8390@gmail.com>
2024-10-13 17:48:41 +0900

    add: img api client

--- src/app/_api/client.ts
+++ src/app/_api/client.ts
@@ -1,13 +1,18 @@
 import { makeApi, Zodios, type ZodiosOptions } from "@zodios/core";
 import { z } from "zod";
 
-const PostUserBody = z.object({ user_name: z.string() }).passthrough();
+const PostUserBody = z
+  .object({
+    user_name: z.string(),
+    user_icon_uuid: z.union([z.string(), z.null()]),
+  })
+  .passthrough();
 const SelfUserRes = z
   .object({
-    user_id: z.string(),
+    user_id: z.string().uuid(),
     user_name: z.string(),
     user_screen_id: z.string(),
-    user_icon_uuid: z.string(),
+    user_icon_uuid: z.union([z.string(), z.null()]),
     user_date: z.string().datetime({ offset: true }),
     user_mail: z.string(),
   })
@@ -30,6 +35,19 @@ const TokenInfo = z
 const TokenRes = z
   .object({ access: TokenInfo, refresh: TokenInfo })
   .passthrough();
+const ImgTokenRes = z.object({ upload: TokenInfo }).passthrough();
+const name = z.union([z.array(z.string()), z.null()]).optional();
+const post_by = z.union([z.array(z.string().uuid()), z.null()]).optional();
+const start_datetime = z.union([z.string(), z.null()]).optional();
+const following = z.union([z.boolean(), z.null()]).optional();
+const read_limit_number = z.union([z.number(), z.null()]).optional();
+const PostCreatorBody = z
+  .object({
+    user_id: z.string().uuid(),
+    contact_address: z.string(),
+    transfer_target: z.string(),
+  })
+  .passthrough();
 
 export const schemas = {
   PostUserBody,
@@ -39,9 +57,37 @@ export const schemas = {
   PostTokenBody,
   TokenInfo,
   TokenRes,
+  ImgTokenRes,
+  name,
+  post_by,
+  start_datetime,
+  following,
+  read_limit_number,
+  PostCreatorBody,
 };
 
 const endpoints = makeApi([
+  {
+    method: "post",
+    path: "/api/creator",
+    alias: "post_creator_api_creator_post",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: PostCreatorBody,
+      },
+    ],
+    response: z.unknown(),
+    errors: [
+      {
+        status: 422,
+        description: `Validation Error`,
+        schema: HTTPValidationError,
+      },
+    ],
+  },
   {
     method: "post",
     path: "/api/token",
@@ -65,6 +111,13 @@ const endpoints = makeApi([
   },
   {
     method: "get",
+    path: "/api/token/image",
+    alias: "image_token_api_token_image_get",
+    requestFormat: "json",
+    response: ImgTokenRes,
+  },
+  {
+    method: "get",
     path: "/api/token/refresh",
     alias: "token_refresh_api_token_refresh_get",
     requestFormat: "json",
@@ -79,7 +132,7 @@ const endpoints = makeApi([
       {
         name: "body",
         type: "Body",
-        schema: z.object({ user_name: z.string() }).passthrough(),
+        schema: PostUserBody,
       },
     ],
     response: SelfUserRes,
@@ -104,6 +157,57 @@ const endpoints = makeApi([
     alias: "health_health_get",
     requestFormat: "json",
     response: z.unknown(),
+  },
+  {
+    method: "get",
+    path: "/products/",
+    alias: "read_products_products__get",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "name",
+        type: "Query",
+        schema: name,
+      },
+      {
+        name: "tag",
+        type: "Query",
+        schema: name,
+      },
+      {
+        name: "post_by",
+        type: "Query",
+        schema: post_by,
+      },
+      {
+        name: "start_datetime",
+        type: "Query",
+        schema: start_datetime,
+      },
+      {
+        name: "end_datetime",
+        type: "Query",
+        schema: start_datetime,
+      },
+      {
+        name: "following",
+        type: "Query",
+        schema: following,
+      },
+      {
+        name: "read_limit_number",
+        type: "Query",
+        schema: read_limit_number,
+      },
+    ],
+    response: z.unknown(),
+    errors: [
+      {
+        status: 422,
+        description: `Validation Error`,
+        schema: HTTPValidationError,
+      },
+    ],
   },
 ]);
 
--- src/app/_api/wrapper.ts
+++ src/app/_api/wrapper.ts
@@ -1,4 +1,5 @@
 import {createApiClient} from "@/_api/client";
+import {createApiClient as createImgApiClient} from "@/_api/imgClient";
 import {AxiosRequestConfig} from "axios";
 import {Session} from "next-auth";
 import {ApiResult, Results} from "../../util/err/result";
@@ -6,6 +7,9 @@ import {ErrorIds} from "../../util/err/errorIds";
 import {accessToken} from "@/_api/serverWrapper";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
+const imgBaseUrl = process.env.NEXT_PUBLIC_IMG_BASE_URL as string;
+export const imgApiClient = createImgApiClient(imgBaseUrl, {});
+
 
 export const apiClient = createApiClient(baseUrl, {});
 
@@ -40,7 +44,21 @@ export class ClientContext {
   }
 
   async exec<R>(func: (opt: AxiosRequestConfig) => Promise<R>, opt?: AxiosRequestConfig): Promise<ApiResult<R>> {
-    return this.execBody((body, opt1) => func(opt1), null, opt)
+    return this.execBody((_, opt1) => func(opt1), null, opt)
   }
 
+  async uploadImg(file: File) {
+    const token = await this.exec(apiClient.image_token_api_token_image_get)
+    if (token.error) return token
+
+    return await imgApiClient.upload_image_upload__post(
+      {file: file},
+      {
+        headers: {Authorization: token.value.upload.token}
+      }
+    ).then(value => Results.createSuccessResult(value))
+      .catch(reason => {
+        return Results.errResultByReason(reason, ErrorIds.ApiError)
+      })
+  }
 }
