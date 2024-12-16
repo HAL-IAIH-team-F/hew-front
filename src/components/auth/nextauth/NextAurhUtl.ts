d55a903af8df13de7e961b3cab839d47b428ab5d
kigawa <kigawa.8390@gmail.com>
2024-12-16 16:02:29 +0900

    rm: next auth

--- src/components/auth/nextauth/NextAurhUtl.ts
+++
@@ -1,8 +0,0 @@
-import {SessionContextValue} from "next-auth/react";
-import {NextAuthUpdateSession} from "~/auth/nextauth/NextAuthUpdateSession";
-
-export namespace NextAurhUtl {
-  export async function updateSession(session: SessionContextValue, update: NextAuthUpdateSession) {
-    return await session.update(update)
-  }
-}
