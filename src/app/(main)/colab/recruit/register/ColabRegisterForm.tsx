ea862be85d14c19e6c05665ff53e23c7fbc160d5
atsuya <f.atsuya.personal@gmail.com>
2025-01-17 14:12:33 +0900

    wip

--- src/app/(main)/colab/recruit/register/ColabRegisterForm.tsx
+++ src/app/(main)/colab/recruit/register/ColabRegisterForm.tsx
@@ -7,13 +7,13 @@ import {StyledButton} from "../../../../../util/form/element/StyledButton";
 import {Api} from "~/api/context/Api";
 
 export default function ColabRegisterForm({...props}: ColabRegisterFormProps,) {
-  const clientContext = useClientContextState()
+    const clientContext = useClientContextState()
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
     // ボタンの有効/無料を判定
-    const isButtonDisabled =!title.trim() || !description.trim(); // ボタンの有効/無効判定
+    const isButtonDisabled =!title.trim() || !description.trim();
     console.log("isButtonDisabled:", isButtonDisabled);
 
     const handleSubmit = async () => {
@@ -56,131 +56,144 @@ export default function ColabRegisterForm({...props}: ColabRegisterFormProps,) {
         }
     };
 
-  return (
-    <StyledForm onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
-        <div className="w-full h-screen bg-ocean-wave bg-[length:200%_200%] animate-wave">
-            <div>
-                <h1 className="pt-24 mb-44 text-center text-5xl text-white">コラボ募集</h1>
-            </div>
-            <div style={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-            }}
-                 className="mb-12 m-auto"
-            >
-                <div style={{position: "relative", width: "100%", minWidth: "600px"}}>
-                    <StyledInput
-                        name="title"
-                        label="募集タイトル"
-                        value={title}
-                        onChange={(e) => {
-                            setTitle(e.target.value)
-                            if (errors.title) setErrors((prev) => ({...prev, title: ""}));
+    return (
+        <StyledForm onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
+            <div className="w-full h-screen bg-ocean-wave bg-[length:200%_200%] animate-wave relative">
+                {/* 背景に複数の円を追加 */}
+                {Array.from({ length: 100 }).map((_, index) => (
+                    <div
+                        key={index}
+                        className="animate-rise bg-blue w-8 h-8 rounded-full absolute"
+                        style={{
+                            top: `${Math.random() * 100}%`, // ランダムな位置に配置
+                            left: `${Math.random() * 100}%`,
+                            animationDelay: `${Math.random() * 5}s`, // ランダムなアニメーションの遅延
                         }}
-                        maxLength={20}
-                        placeholder="20文字以内で入力してください"
-                        className=""
-                        style={{height: "40px", width: "100%"}} // 入力フィールドを幅100%にする
-                    />
-                    <p className="text-gray-500 text-sm"
-                       style={{
-                           position: "absolute",
-                           bottom: "-20px",
-                           right: "100px",
-                       }}
-                    >
-                        {title.length}/20 文字
-                    </p>
-                    <p className="mt-2 text-red-500"
-                       style={{
-                           width: "240px",
-                           position: "absolute",
-                           bottom: "-50px",
-                           right: "90px",
-                       }}
-                    >
-                        {errors.title}
-                    </p>
+                    ></div>
+                ))}
+
+                <div>
+                    <h1 className="pt-24 mb-44 text-center text-5xl text-white">コラボ募集</h1>
                 </div>
-            </div>
+                <div style={{
+                    position: "relative",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                }}
+                     className="mb-12 m-auto"
+                >
+                    <div style={{position: "relative",
+                                width: "100%",
+                                minWidth: "600px",
+                                }}>
+                        <StyledInput
+                            name="title"
+                            label="募集タイトル"
+                            value={title}
+                            onChange={(e) => {
+                                setTitle(e.target.value)
+                                if (errors.title) setErrors((prev) => ({...prev, title: ""}));
+                            }}
+                            maxLength={20}
+                            placeholder="20文字以内で入力してください"
+                            className=""
+                            style={{height: "40px", width: "100%"}} // 入力フィールドを幅100%にする
+                        />
+                        <p className="text-gray-500 text-sm"
+                           style={{
+                               position: "absolute",
+                               bottom: "10px",
+                               right: "100px",
+                           }}
+                        >
+                            {title.length}/20 文字
+                        </p>
+                        <p className="mt-2 text-red-500"
+                           style={{
+                               width: "240px",
+                               position: "absolute",
+                               bottom: "-20px",
+                               left: "220px",
+                           }}
+                        >
+                            {errors.title}
+                        </p>
+                    </div>
+                </div>
 
 
-            <div style={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "100%",
-                    }}
-                         className="mb-12 m-auto"
-                    >
-                        <div style={{position: "relative", width: "100%", minWidth: "600px"}}>
-                            <StyledInput
-                                as="textarea"
-                                name="description"
-                                label="募集要項"
-                                value={description}
-                                onChange={(e) => {
-                                    setDescription(e.target.value);
-                                    if (errors.description) setErrors((prev) => ({...prev, description: ""}));
-                                }}
-                                maxLength={200}
-                                placeholder="200文字以内で入力してください"
-                                className=""
-                                style={{
-                                    height: "200px",
-                                    width: "100%",
-                                    textAlign: "left",
-                                    verticalAlign: "top",
-                                    // padding: "10px",
-                                    resize: "none", // ユーザーによるサイズ変更無効
-                                    overflow: "auto",
-                                    whiteSpace: "pre-wrap", //　改行有効
-                                    wordWrap: "break-word", // 長い単語折り返し
-                                }}
-                            />
-                            <p className="text-gray-500 text-sm"
+                <div style={{
+                    position: "relative",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                }}
+                     className="mb-12 m-auto"
+                >
+                    <div style={{position: "relative", width: "100%", minWidth: "600px"}}>
+                        <StyledInput
+                            as="textarea"
+                            name="description"
+                            label="募集要項"
+                            value={description}
+                            onChange={(e) => {
+                                setDescription(e.target.value);
+                                if (errors.description) setErrors((prev) => ({...prev, description: ""}));
+                            }}
+                            maxLength={200}
+                            placeholder="200文字以内で入力してください"
+                            className=""
+                            style={{
+                                height: "200px",
+                                width: "100%",
+                                textAlign: "left",
+                                verticalAlign: "top",
+                                resize: "none", // ユーザーによるサイズ変更無効
+                                overflow: "auto",
+                                whiteSpace: "pre-wrap", //　改行有効
+                                wordWrap: "break-word", // 長い単語折り返し
+                            }}
+                        />
+                        <p className="text-gray-500 text-sm"
+                           style={{
+                               position: "absolute",
+                               bottom: "10px",
+                               right: "100px",
+                           }}
+                        >
+                            {description.length}/200 文字
+                        </p>
+                        {errors.description &&
+                            <p className="mt-2 text-red-500"
                                style={{
+                                   width: "240px",
                                    position: "absolute",
                                    bottom: "-20px",
-                                   right: "100px",
+                                   left: "220px",
                                }}
                             >
-                                {description.length}/200 文字
+                                {errors.description}
                             </p>
-                            {errors.description &&
-                                <p className="mt-2 text-red-500"
-                                   style={{
-                                       width: "240px",
-                                       position: "absolute",
-                                       bottom: "-50px",
-                                       right: "65px",
-                                   }}
-                                >
-                                    {errors.description}
-                                </p>
-                            }
-                        </div>
+                        }
                     </div>
-                    <StyledButton
-                        className="mt-5 disabled:opacity-50 disabled:cursor-not-allowed"
-                        disabled={isButtonDisabled}
-                        onClick={handleSubmit}
-                    >
-                        募集する
-                    </StyledButton>
-                    {errors.submit && <p className="text-red-500 mt-2">{errors.submit}</p>}
-                {/*</div>*/}
+                </div>
+                <StyledButton
+                    className="mt-5 disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={isButtonDisabled}
+                    onClick={handleSubmit}
+                >
+                    募集する
+                </StyledButton>
+                {errors.submit && <p className="text-red-500 mt-2">{errors.submit}</p>}
             </div>
-    </StyledForm>
-);
+        </StyledForm>
+    );
 }
 
 export interface ColabRegisterFormProps {
 }
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     extend: {
       colors: {
         white: "#F8F8F8",
-        blue: "#327AD9",
+        blue: "rgba(5,41,63,0.73)",
         lightBlue: "#52D9CB",
         crystal: "rgb(0,0,0)",
         backcolor: "#05253F",
@@ -14,21 +14,36 @@ const config: Config = {
         lightGray: "#747989",
       },
       keyframes: {
-        wave: {
-          "0%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-          "100%": { backgroundPosition: "0% 50%" },
+        // wave: {
+        //   "0%": { backgroundPosition: "0% 50%" },
+        //   "50%": { backgroundPosition: "100% 50%" },
+        //   "100%": { backgroundPosition: "0% 50%" },
+        // },
+        rise: {
+          "0%": {
+            transform: "translateY(100%)",
+            opacity: "0",
+          },
+          "50%": {
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translateY(-100%)",
+            opacity: "0",
+          },
         },
       },
       animation: {
         wave: "wave 10s ease infinite",
+        rise: "rise 5s ease-in-out infinite",
       },
-      backgroundImage: {
-        "ocean-wave":
-            "linear-gradient(90deg, #0C4C81, #0B4372, #08385F, #042948, #05253F)",
-      },
+      // backgroundImage: {
+      //   "ocean-wave":
+      //       "linear-gradient(90deg, #0C4C81, #0B4372, #08385F, #042948, #05253F)",
+      // },
     },
   },
   plugins: [],
 };
+
 export default config;
