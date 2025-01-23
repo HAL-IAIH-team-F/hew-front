e48282a9e8191f0bfbcba3933696887b96d5222f
atsuya <f.atsuya.personal@gmail.com>
2025-01-23 19:28:32 +0900

    ClientContextStateがないよって言われたので、useClientStateに戻して開発します

--- src/app/(main)/colab/recruit/register/ColabRegisterForm.tsx
+++ src/app/(main)/colab/recruit/register/ColabRegisterForm.tsx
@@ -1,13 +1,13 @@
 "use client"
 import { useState, useEffect } from "react";
-import {useClientContextState} from "~/api/context/ClientContextProvider";
+import {useClientState} from "~/api/context/ClientContextProvider";
 import {StyledForm} from "../../../../../util/form/element/StyledForm";
 import {StyledInput} from "../../../../../util/form/element/StyledInput";
 import {StyledButton} from "../../../../../util/form/element/StyledButton";
 import {Api} from "~/api/context/Api";
 
 export default function ColabRegisterForm({...props}: ColabRegisterFormProps,) {
-    const clientContext = useClientContextState();
+    const clientContext = useClientState()
     const [title, setTitle] = useState("");
     const [showPlaceholder, setShowPlaceholder] = useState(false);
     const [description, setDescription] = useState("");
