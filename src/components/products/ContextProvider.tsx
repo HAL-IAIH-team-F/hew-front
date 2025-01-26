e1a72cbb10fe67b7f487a239a5472f8170c02c12
mao <90289410+injectxr@users.noreply.github.com>
2025-01-27 01:55:59 +0900

    wp

--- src/components/products/ContextProvider.tsx
+++ src/components/products/ContextProvider.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useWindowSize } from '@/_hook/useWindowSize';
+import {usePathname, useRouter} from "next/navigation";
 import React, {createContext, useContext, useEffect, useMemo, useState} from 'react';
 import internal from 'stream';
-
+import {Routes} from "@/Routes";
 // Contextの型定義
 interface ProductContextType {
   productId: string;
@@ -23,18 +24,41 @@ export const ProductProvider: React.FC<{ children: React.ReactNode }> = ({childr
   const [productId, setProductId] = useState<string>("none");
   const [isProductOpen, setisProductOpen] = useState<boolean>(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+  const [isPathTimeline, setIsPathTimeline] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  
+  const [window, setWindow] = useState<boolean>(false);
+  const router = useRouter()
+  const pathname = usePathname(); // 現在のURLパスを取得
   const toggleProductWindow = () => setisProductOpen((prev) => !prev);
   const size = useWindowSize()
   useEffect(() => {
-    console.log('Window width:', size.width);
-    if (size.width <= 794 && isProductOpen && isSidebarOpen) {
-      console.log('Closing product window');
-      setisProductOpen(false);
+    if (pathname !== "/") {
+      if (isProductOpen && isMobile)
+      {
+        setProductId("none")
+        setisProductOpen(false)
+      }else{
+        return setIsPathTimeline(true)
+      }
+    }
+    return setIsPathTimeline(false) 
+  },[pathname])
+  useEffect(() => {
+    if (isMobile && !isPathTimeline && isProductOpen) {
+      router.push(Routes.timeline)
     }
   }, [size, isProductOpen, isSidebarOpen]);
+
+  useEffect(() => {
+    if (size.width <= 1280) {
+      setIsMobile(true)
+    }else{
+      setIsMobile(false)
+    }
+  },[size])
+
+
+
 
   return (
     <ProductContext.Provider
