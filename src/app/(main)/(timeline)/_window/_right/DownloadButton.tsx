fb09ee5e85ed9ef551b0a7dd80399d54dc020ab3
kigawa <kigawa.8390@gmail.com>
2025-02-10 03:14:20 +0900

    add; download

---
+++ src/app/(main)/(timeline)/_window/_right/DownloadButton.tsx
@@ -0,0 +1,31 @@
+import {PurchaseInfo} from "@/(main)/search/sample/ProductRes";
+import {useEffect, useState} from "react";
+import {Img} from "~/api/context/Api";
+import {ErrorData} from "../../../../../util/err/err";
+import {ErrorMessage} from "../../../../../util/err/ErrorMessage";
+
+export default function DownloadButton(
+    {
+        purchaseInfo,
+    }: {
+        purchaseInfo: PurchaseInfo
+    },
+) {
+    const [url, setUrl] = useState<Img>()
+    const [err, setErr] = useState<ErrorData>()
+    useEffect(() => {
+        setErr(undefined)
+        Img.create(purchaseInfo.content_uuid, purchaseInfo.token.token).then(value => {
+            if (value.error) return setErr(value.error)
+            setUrl(value.success)
+        })
+    }, [purchaseInfo]);
+    return (
+        <>
+            <a href={url?.strUrl(true)} target={"_blank"}>
+                download
+            </a>
+            <ErrorMessage error={err}/>
+        </>
+    )
+}
